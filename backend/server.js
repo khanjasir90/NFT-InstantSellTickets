c7367bcd93832fa599b342a2336ce90b5c913ffa
khanjasir90 <khanmohdjasir@gmail.com>
2023-01-01 13:05:02 +0530

    added register and login api and db configuration
    Signed-off-by: khanjasir90 <khanmohdjasir@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+require('dotenv').config();
+require('./db_config')
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+}
+
 
 const eventRouter = require('./routes/eventRoute');
 const userRouter  = require('./routes/userRoute');
 const homeRouter = require('./routes/homeRoute');
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/api', eventRouter);
 app.use('/api', userRouter);
 app.use('/api', homeRouter);
 
 
-app.listen(PORT, () => console.log('Server running on port '+PORT));
+app.listen(process.env.PORT, () => console.log(`server running on ${process.env.PORT}`));
