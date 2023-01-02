10fe843aa70855b432549837b70410c7cf98fb4a
khanjasir90 <khanmohdjasir@gmail.com>
2023-01-02 08:00:16 +0530

    added events api
    Signed-off-by: khanjasir90 <khanmohdjasir@gmail.com>

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
+    setEvent,
     getAllEvents,
     getSingleEvent
 } = require('../controller/eventsController');
 
+router.post('/events', setEvent);
 router.get('/events', getAllEvents);
 router.get('/events/:id', getSingleEvent);
 
